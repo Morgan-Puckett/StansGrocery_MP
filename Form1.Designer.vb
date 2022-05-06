ffa331b0b3b00f021f23c1cb8b8c1753e62fa857
Morgan Puckett <morganpuckett02@gmail.com>
2022-05-06 02:00:10 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,188 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.FilterComboBox = New System.Windows.Forms.ComboBox()
+        Me.DisplayLabel = New System.Windows.Forms.Label()
+        Me.FilterGroupBox = New System.Windows.Forms.GroupBox()
+        Me.FilterByCategoryRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FilterByAisleRadioButton = New System.Windows.Forms.RadioButton()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ResetButton = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FilterGroupBox.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DisplayListBox
+        '
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 20
+        Me.DisplayListBox.Location = New System.Drawing.Point(535, 11)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(253, 424)
+        Me.DisplayListBox.TabIndex = 0
+        '
+        'SearchTextBox
+        '
+        Me.SearchTextBox.Location = New System.Drawing.Point(106, 57)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(125, 27)
+        Me.SearchTextBox.TabIndex = 2
+        '
+        'FilterComboBox
+        '
+        Me.FilterComboBox.FormattingEnabled = True
+        Me.FilterComboBox.Location = New System.Drawing.Point(63, 171)
+        Me.FilterComboBox.Name = "FilterComboBox"
+        Me.FilterComboBox.Size = New System.Drawing.Size(151, 28)
+        Me.FilterComboBox.TabIndex = 3
+        '
+        'DisplayLabel
+        '
+        Me.DisplayLabel.AutoSize = True
+        Me.DisplayLabel.Location = New System.Drawing.Point(12, 139)
+        Me.DisplayLabel.Name = "DisplayLabel"
+        Me.DisplayLabel.Size = New System.Drawing.Size(99, 20)
+        Me.DisplayLabel.TabIndex = 4
+        Me.DisplayLabel.Text = "You will find..."
+        '
+        'FilterGroupBox
+        '
+        Me.FilterGroupBox.Controls.Add(Me.FilterByCategoryRadioButton)
+        Me.FilterGroupBox.Controls.Add(Me.FilterByAisleRadioButton)
+        Me.FilterGroupBox.Location = New System.Drawing.Point(278, 74)
+        Me.FilterGroupBox.Name = "FilterGroupBox"
+        Me.FilterGroupBox.Size = New System.Drawing.Size(162, 125)
+        Me.FilterGroupBox.TabIndex = 5
+        Me.FilterGroupBox.TabStop = False
+        Me.FilterGroupBox.Text = "FilterGroupBox"
+        '
+        'FilterByCategoryRadioButton
+        '
+        Me.FilterByCategoryRadioButton.AutoSize = True
+        Me.FilterByCategoryRadioButton.Location = New System.Drawing.Point(23, 75)
+        Me.FilterByCategoryRadioButton.Name = "FilterByCategoryRadioButton"
+        Me.FilterByCategoryRadioButton.Size = New System.Drawing.Size(147, 24)
+        Me.FilterByCategoryRadioButton.TabIndex = 1
+        Me.FilterByCategoryRadioButton.TabStop = True
+        Me.FilterByCategoryRadioButton.Text = "Filter by Category"
+        Me.FilterByCategoryRadioButton.UseVisualStyleBackColor = True
+        '
+        'FilterByAisleRadioButton
+        '
+        Me.FilterByAisleRadioButton.AutoSize = True
+        Me.FilterByAisleRadioButton.Location = New System.Drawing.Point(23, 35)
+        Me.FilterByAisleRadioButton.Name = "FilterByAisleRadioButton"
+        Me.FilterByAisleRadioButton.Size = New System.Drawing.Size(119, 24)
+        Me.FilterByAisleRadioButton.TabIndex = 0
+        Me.FilterByAisleRadioButton.TabStop = True
+        Me.FilterByAisleRadioButton.Text = "Filter by Aisle"
+        Me.FilterByAisleRadioButton.UseVisualStyleBackColor = True
+        '
+        'SearchButton
+        '
+        Me.SearchButton.Location = New System.Drawing.Point(106, 95)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(72, 40)
+        Me.SearchButton.TabIndex = 6
+        Me.SearchButton.Text = "Search "
+        Me.SearchButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(46, 226)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 20)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Label1"
+        '
+        'ResetButton
+        '
+        Me.ResetButton.Location = New System.Drawing.Point(107, 399)
+        Me.ResetButton.Name = "ResetButton"
+        Me.ResetButton.Size = New System.Drawing.Size(70, 15)
+        Me.ResetButton.TabIndex = 8
+        Me.ResetButton.Text = "Reset"
+        Me.ResetButton.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 263)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(53, 20)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 293)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(53, 20)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Label3"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 451)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.ResetButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.FilterGroupBox)
+        Me.Controls.Add(Me.DisplayLabel)
+        Me.Controls.Add(Me.FilterComboBox)
+        Me.Controls.Add(Me.SearchTextBox)
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Name = "Form1"
+        Me.Text = "Stans Grocery"
+        Me.FilterGroupBox.ResumeLayout(False)
+        Me.FilterGroupBox.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents FilterComboBox As ComboBox
+    Friend WithEvents DisplayLabel As Label
+    Friend WithEvents FilterGroupBox As GroupBox
+    Friend WithEvents FilterByCategoryRadioButton As RadioButton
+    Friend WithEvents FilterByAisleRadioButton As RadioButton
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ResetButton As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,195 @@
+﻿'Morgan Puckett
+'RCET 0265
+'May 6th 2022
+'Stans Grocery
+'
+
+
+Option Strict On
+Option Explicit On
+
+'I cant rename any forms without major issues, is this a computer problem?
+Public Class Form1
+    Dim food(300, 2) As String
+    Dim usable(300, 2) As String
+    Dim filename As String = "C:\Users\morga\OneDrive\VB\StansGrocery_MP\Grocery.txt" ' set up own path on machine
+
+    'Searchs file for keywords
+    Private Sub SearchButton_Click() Handles SearchButton.Click
+        FilterByCategoryRadioButton.Checked = True
+        If SearchTextBox.Text = "zzz" Then
+            Me.Close()
+        Else
+            CheckFile(SearchTextBox.Text)
+        End If
+        Select Case DisplayListBox.Items.Count
+            Case < 1
+                DisplayLabel.Text = $"Sorry no matches for '{SearchTextBox.Text}'"
+            Case = 1
+                DisplayLabel.Text = $"{DisplayListBox.Items.Count} product"
+            Case >= 2
+                DisplayLabel.Text = $"{DisplayListBox.Items.Count} products"
+        End Select
+
+    End Sub
+
+    'Shows file in listed format with names only
+    Private Sub DisplayListBox_Click() Handles DisplayListBox.Click
+
+        Try
+            If DisplayListBox.SelectedItem.ToString() <> Nothing Then
+                DisplayLabel.Text = ($" You will find {DisplayListBox.SelectedItem.ToString()} on aisle {CheckLocation(SearchTextBox.Text)} ")
+
+            End If
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    'Loads and resets form/file
+    Private Sub StansGroceryForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ReadFile()
+        Reset()
+    End Sub
+
+    'Filters
+    Private Sub FilterSelect() Handles FilterByAisleRadioButton.CheckedChanged, FilterByCategoryRadioButton.CheckedChanged
+        Dim er() As String
+        Dim re As String
+        Dim i As Integer
+        Select Case True
+            Case FilterByAisleRadioButton.Checked
+                re = "##LOC"
+                i = 1
+
+            Case FilterByCategoryRadioButton.Checked
+                re = "%%CAT"
+                i = 2
+        End Select
+        FilterComboBox.Items.Clear()
+        FilterComboBox.Items.Add(" Show All")
+        FilterComboBox.SelectedItem = " Show All"
+
+        For i = LBound(food) To UBound(food)
+            Try
+                er = Split(food(i, i), re)
+                If FilterComboBox.Items.Contains(er(1)) = False And er(1) <> "" Then   '.PadLeft(2)) = False And er(1) <> "" Then
+                    FilterComboBox.Items.Add(er(1))
+
+                End If
+            Catch ex As Exception
+
+            End Try
+        Next
+        FilterComboBox.Sorted = True
+    End Sub
+
+    'Applies filters necessary
+    Private Sub FilterComboBox_SelectedindexChanged() Handles FilterComboBox.SelectedIndexChanged
+        '
+        If FilterByCategoryRadioButton.Checked = True Then
+            DisplayLabel.Text = $"{FilterComboBox.Text} products"
+        End If
+        If FilterByAisleRadioButton.Checked = True And FilterComboBox.Text <> "Show All" Then
+            DisplayLabel.Text = $"{FilterComboBox.Text} Aisle products  "
+        End If
+
+        If FilterComboBox.Text = " Show All" Then
+            CheckFile(" ")
+        Else
+            CheckFile(FilterComboBox.Text)
+        End If
+
+    End Sub
+    Private Sub ResetButton_Click(sender As Object, e As EventArgs) Handles ResetButton.Click
+        Reset()
+    End Sub
+
+    'Finds Location of the desired item
+    'Fair warning it doesn't retreive the right one- I cant figure out why
+    Function CheckLocation(desired As String) As String
+        Dim itemLocation() As String
+        Dim i As Integer = 0
+
+        i = DisplayListBox.SelectedIndex + 1
+        itemLocation = Split(food(i, 1), "##LOC")
+
+        Label1.Text = itemLocation(0)
+        Label2.Text = itemLocation(1)
+        Label3.Text = itemLocation(2)
+
+        If itemLocation(1) = "" Then
+            itemLocation(1) = "N/A"
+        End If
+
+        Return itemLocation(1)
+        itemLocation(1) = ""
+        itemLocation(2) = ""
+        itemLocation(0) = ""
+    End Function
+
+    'Sets defaults of program
+    Sub Reset()
+        SearchTextBox.Text = Nothing
+        FilterByCategoryRadioButton.Checked = True
+        FilterComboBox.Text = (" Show All")
+        SearchButton_Click()
+    End Sub
+
+    'Searches File for KeyWords
+    Sub CheckFile(desired As String)
+
+        Dim current() As String
+        Dim loc() As String
+        Dim cat() As String
+        Dim tempRec As String
+        DisplayListBox.Items.Clear()
+
+        For i = LBound(food) To UBound(food)
+            tempRec = ($"{food(i, 0)} {food(i, 1)} {food(i, 2)} ")
+
+            current = Split(food(i, 0), "$$ITM")
+            loc = Split(food(i, 1), "##LOC")
+            cat = Split(food(i, 2), "%%CAT")
+            Try
+                Console.WriteLine(current(1))
+                If InStr(tempRec, desired, CompareMethod.Text) > 0 And InStr(tempRec, desired, CompareMethod.Text) <> 0 Then
+                    DisplayListBox.Items.Add($"{current(1)}")
+                    usable(i, 0) = current(1)
+                    usable(i, 1) = loc(1)
+                    usable(i, 2) = cat(1)
+
+                End If
+            Catch ex As Exception
+                Console.WriteLine("oops")
+            End Try
+        Next
+        DisplayListBox.Sorted = True
+        DisplayListBox.Items.Remove("")
+
+    End Sub
+
+    'Opens and Reads File
+    Sub ReadFile()
+        'Clear listbox each time called for neatness
+        DisplayListBox.Items.Clear()
+        Dim er As String
+        Dim itemNumber As Integer
+        Try
+            FileOpen(1, filename, OpenMode.Input)
+            Do Until EOF(1)
+                For lineNumber = 0 To 2
+
+                    Input(1, er)
+                    food(itemNumber, lineNumber) = er
+
+                Next
+                itemNumber += 1
+            Loop
+            FileClose(1)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+End Class
